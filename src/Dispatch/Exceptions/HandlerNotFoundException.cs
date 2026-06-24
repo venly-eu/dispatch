524ecfc2876ed1096b2f1533ea26649005424fa2
Oliver Rasmussen <oliver.rasmussen@outlook.dk>
2026-06-24 20:30:22 +0200

    Update exception msg

--- src/Dispatch/Exceptions/HandlerNotFoundException.cs
+++ src/Dispatch/Exceptions/HandlerNotFoundException.cs
@@ -3,6 +3,7 @@ namespace Vesia.Dispatch.Exceptions;
 public class HandlerNotFoundException : Exception
 {
     public HandlerNotFoundException(string handlerName) 
-        : base($"No handler registered for '{handlerName}'")
+        : base($"No handler registered for '{handlerName}'" +
+               $"- Make sure you called AddDispatch() in the same project as your handlers.")
     { }
 }
